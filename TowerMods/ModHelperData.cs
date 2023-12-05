0112f3e9b134ec0c2063e55d240fc772c3209b00
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-05 09:17:12 -0500

    Added ModHelperData

---
+++ TowerMods/ModHelperData.cs
@@ -0,0 +1,17 @@
+namespace DatJaneDoeTowerPack;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "39.0";
+    public const string Version = "1.0.2";
+    public const string Name = "DatJaneDoeTowerPack 1";
+
+    public const string Description = "Adds Necromancer, Pizza, Soldier, and Teacher towers to the game";
+
+    public const string Icon = "pack1";
+    public const string RepoOwner = "DatJaneDoe";
+    public const string RepoName = "BTD6-Mods";
+    public const string DllName = "DJDTowerPack.dll";
+	
+	public const string SubPath = "DJDTowerPack";
+}
