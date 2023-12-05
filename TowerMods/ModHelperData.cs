ea8e456d5f36faca59455775a4b0f0830cf55828
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-04 22:06:34 -0500

    Added ModHelperData
    Trying to get DJDTowerPack to show on the mod browser

---
+++ TowerMods/ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace DatJaneDoeTowerPack;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "39.0";
+    public const string Version = "1.0.2";
+    public const string Name = "DatJaneDoeTowerPack 1";
+
+    public const string Description = "Adds Necromancer, Pizza, Soldier, and Teacher towers to the game";
+
+    public const string RepoOwner = "DatJaneDoe";
+    public const string RepoName = "BTD6-Mods";
+    public const string DllName = "DJDTowerPack.dll";
+}
